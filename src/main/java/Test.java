1c1ad3bdf1bc25a7ea504a8d5975cddf19f58546
WangKai <275892441>
2018-05-04 18:01:03 +0800

    dd

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,11 +1,6 @@
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
-import io.reactivex.Observer;
+import io.reactivex.*;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.BiFunction;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
+import io.reactivex.functions.*;
 import kotlin.text.StringsKt;
 
 import java.io.Serializable;
@@ -16,27 +11,56 @@ import java.util.concurrent.TimeUnit;
 public class Test {
     public static void main(String[] args) {
 
-        Observable.range(1, 5)
-                .doOnNext(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Exception {
-                        if (integer == 4) {
-                            throw new Exception("dddd");
-                        }
-                    }
-                })
-                .retry(1)
-                .subscribe(new Consumer<Integer>() {
-                    @Override
-                    public void accept(Integer integer) throws Exception {
-                        System.out.println(integer);
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        System.out.println(throwable.getMessage());
-                    }
-                });
+       Observable.create(new ObservableOnSubscribe<Integer>() {
+           @Override
+           public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+               emitter.onNext(1);
+               emitter.onError(new Error("sksk"));
+               emitter.onNext(2);
+               emitter.onComplete();
+           }
+       })
+               .retryWhen(new Function<Observable<Throwable>, ObservableSource<?>>() {
+                   @Override
+                   public ObservableSource<?> apply(Observable<Throwable> throwableObservable) throws Exception {
+                       return throwableObservable.;
+                   }
+               })
+//               .onExceptionResumeNext(new Observable<Integer>() {
+//                   @Override
+//                   protected void subscribeActual(Observer<? super Integer> observer) {
+//                       observer.onNext(11);
+//                       observer.onComplete();
+//                   }
+//               })
+//               .onErrorResumeNext(new Observable<Integer>() {
+//                   @Override
+//                   protected void subscribeActual(Observer<? super Integer> observer) {
+//                       observer.onNext(21);
+//                       observer.onComplete();
+//                   }
+//               })
+               .subscribe(new Observer<Integer>() {
+                   @Override
+                   public void onSubscribe(Disposable d) {
+
+                   }
+
+                   @Override
+                   public void onNext(Integer integer) {
+                       System.out.println(integer);
+                   }
+
+                   @Override
+                   public void onError(Throwable e) {
+                       System.out.println(e.getMessage());
+                   }
+
+                   @Override
+                   public void onComplete() {
+                       System.out.println("onComplete");
+                   }
+               });
 
 
         try {
